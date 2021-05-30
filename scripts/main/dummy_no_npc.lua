91257e18645bc5094c34f26982679eeb64cd2cfd
64Will64 <49082269+64Will64@users.noreply.github.com>
2021-05-30 17:11:27 -0400

    Update dummy_no_npc.lua

--- scripts/main/dummy_no_npc.lua
+++ scripts/main/dummy_no_npc.lua
@@ -1,7 +1,4 @@
 print("dummy but its not an npc apparently lmaoo")
-for i,v in pairs(game.Players.LocalPlayer.Character) do
-    if v:IsA("Shirt") or v:IsA("Pants") then
-        v:Destroy()
-    end
-end
+game.Players.LocalPlayer.Character.Shirt:Destroy()
+game.Players.LocalPlayer.Character.Pants:Destroy()
 game.Players.LocalPlayer.Character.Head.Mesh:Destroy()

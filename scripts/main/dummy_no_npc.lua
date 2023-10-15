58889f881d45b52cdc2b1e61776dc04f8e294cb4
Will <49082269+64Will64@users.noreply.github.com>
2023-10-15 14:39:58 -0400

    Delete scripts/main/dummy_no_npc.lua

--- scripts/main/dummy_no_npc.lua
+++
@@ -1,4 +0,0 @@
-print("dummy but its not an npc apparently lmaoo")
-game.Players.LocalPlayer.Character.Shirt:Destroy()
-game.Players.LocalPlayer.Character.Pants:Destroy()
-game.Players.LocalPlayer.Character.Head.Mesh:Destroy()
